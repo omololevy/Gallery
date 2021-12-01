c4466bb9b0d97a37ae7715c70c00f7875c899f35
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-12-01 22:28:22 +0300

    Create the url for a search in this file

--- photos/urls.py
+++ photos/urls.py
@@ -9,7 +9,8 @@ urlpatterns=[
 re_path(r'^$',views.home,name='home'),
 re_path(r'^filter/(\d+)',views.filter_location,name='filter_location'),
 re_path(r'^search/', views.search_results,name='search_results')   ,
-path('uploads',views.upload_picture,name="upload_picture")
+path('uploads',views.upload_picture,name="upload_picture"),
+path('select', views.query_image, name='query_image')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL,document_root= settings.MEDIA_ROOT)
